07e005438ebfa64df2e7a72a297b46be69449bb7
Frederic Rey <Frederic.Rey.Pro@gmail.com>
2016-07-01 14:27:59 +0100

    - Middlewares config done - Camo ODM now configured

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-import express from 'express';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import expressValidator from 'express-validator';
-import flash from 'connect-flash';
 import path from 'path';
+import express from 'express';
 import jade from 'jade';
 import mongodb from 'mongodb';
+import camo from 'camo';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import expressValidator from 'express-validator';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 const MongoStore = require('connect-mongo')(session);
+import flash from 'connect-flash';
+import expressMessages from 'express-messages';
 import env from './env';
 
-// import Routes here
+import routes from './routes/index';
+
+
+// Camo ODM Connection
+const dbUri = 'mongodb://localhost/sportsblog';
+let db;
+camo.connect(dbUri).then((database) => {
+  db = database;
+});
 
 const app = express();
 
@@ -19,12 +29,17 @@ const app = express();
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-// Middlewares
+
+/* **************** *
+      MIDDLEWARES
+ * **************** */
+
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
+
 app.use(expressValidator({
   errorFormatter(param, msg, value) {
     const namespace = param.split('.');
@@ -41,23 +56,42 @@ app.use(expressValidator({
     };
   },
 }));
-app.use(cookieParser(env.COOKIE_SECRET));
+
+// !!! cookie-parser & session must use the same secret to prevent issues
+app.use(cookieParser(env.COOKIE_SESSION_SECRET));
 
 // SessionStore use its own connection!!!!!!!
+// TODO: Try to use to same connection for SessionStore and Camo
 app.use(session({
-  secret: env.SESSION_SECRET,
+  secret: env.COOKIE_SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store: new MongoStore({
-    url: 'mongodb://localhost/sportsblog',
+    url: dbUri,
   }),
 }));
 
-// Public assets
+// Provide to all requests the flash() function which allows to send flash messages
+// req.flash('messageType', 'messageText')
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.messages = expressMessages(req, res);
+  next();
+});
+
+
+/* *************** *
+    PUBLIC ASSETS
+ * *************** */
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Use routes here
+/* *********** *
+      ROUTES
+ * *********** */
+
+app.use('/', routes);
 
 const server = app.listen(env.PORT, () => {
   console.log(`Server is now listening on locahost:${env.PORT}`);
