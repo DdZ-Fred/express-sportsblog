d2b06f9eab67153c76c7091df11ad92156686489
Frederic Rey <Frederic.Rey.Pro@gmail.com>
2016-07-05 15:13:35 +0100

    - Views and routes OK - TODO: Models for Camo ODM

--- routes/articles.js
+++ routes/articles.js
@@ -1,17 +1,26 @@
 import express from 'express';
 const router = express.Router();
 
+/*
+  Show All Articles
+ */
 router.get('/', (req, res) => {
   res.render('articles', {
     title: 'All Articles',
   });
 });
 
+/*
+  Show a single Article (by Id)
+ */
 router.get('/show/:id', (req, res) => {
   // Get article from DB
   res.render('article');
 });
 
+/*
+  Show Articles from particular category
+ */
 router.get('/category/:category_id', (req, res) => {
   res.render('articles');
 });
--- routes/manage.js
+++ routes/manage.js
@@ -1,15 +1,48 @@
 import express from 'express';
 const router = express.Router();
 
+/*
+  ARTICLES
+ */
+
 router.get('/articles', (req, res) => {
   res.render('manage_articles', {
     title: 'Manage Articles',
   });
 });
 
+router.get('/articles/add', (req, res) => {
+  res.render('add_article', {
+    title: 'Add Article',
+  });
+});
+
+router.get('/articles/edit/:article_id', ({ params }, res) => {
+  res.render('edit_article', {
+    title: 'Edit Article',
+  });
+});
+
+
+/*
+  CATEGORIES
+ */
+
 router.get('/categories', (req, res) => {
   res.render('manage_categories', {
     title: 'Manage Categories',
+  });
+});
+
+router.get('/categories/add', (req, res) => {
+  res.render('add_category', {
+    title: 'Add Category',
+  });
+});
+
+router.get('/categories/edit/:category_id', ({ params }, res) => {
+  res.render('edit_category', {
+    title: 'Edit Category',
   });
 });
 
